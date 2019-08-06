f41f3634d0861da4ec016ddc9f50ebe435bd47e4
Zach Smith <zachsmith.sa@gmail.com>
2019-08-06 11:05:47 +0200

    adding a linter

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// app.js
+'use strict'
 import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
@@ -6,18 +6,28 @@ import logger from 'morgan'
 import indexRouter from './routes/index'
 import { buildSchema } from 'graphql'
 import graphqlHTTP from 'express-graphql'
+import { readFileSync } from "fs"
+import { normalize, join } from "path"
+import resolvers from './resolvers'
 
-const app = express()
+// Env config
+const ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:3001']
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001']
-var allowCrossDomain = function(req, res, next) {
+const corsMiddleware = (req, res, next) => {
   const origin = req.headers.origin
-  if (allowedOrigins.includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  const httpVerb = req.method
+
+  if (ALLOWED_ORIGINS.includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin)
+  
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   res.header('Access-Control-Allow-Credentials', true)
-  next()
+  
+  if (httpVerb === 'OPTIONS')  res.send(200)
+  else next()
 }
+
+const app = express()
 
 app.use(logger('dev'))
 
@@ -29,23 +39,32 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
 
 // Serve the router
-app.use(allowCrossDomain)
+app.use(corsMiddleware)
 app.use('/', indexRouter)
 
-// Server GraphQL endpoint
+// Load GraphQL Schema
+const schemaPath = normalize(join(__dirname, './schema.graphql'))
+const schemaDef = readFileSync(schemaPath, {encoding: 'utf8'})
+const schema = buildSchema(schemaDef)
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+// Make GraphiQL available
+app.use(
+  '/graphiql',
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  }),
+)
 
+// GraphQL endpoint
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  }),
+    schema,
+    rootValue: resolvers,
+    graphiql: false
+  })
 )
 
 export default app
