730c82dd93c78f0a670544ac637ca11e69c51b28
Zach Smith <zachsmith.sa@gmail.com>
2019-08-16 16:41:55 +0200

    added build step to copy sql file to dist

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,4 @@
+'use strict'
 import Pool from '../lib/pg'
 import { log } from '../lib/log'
 import { readFileSync } from 'fs'
@@ -16,21 +17,22 @@ const getPool = database =>
 
 export default (async () => {
   let seacrifogPool
+
   const configDbPool = getPool('postgres')
   const dbExists = (await configDbPool.query(
     `select exists(select datname from pg_catalog.pg_database where datname = 'seacrifog');`
   )).rows[0].exists
-
   if (!dbExists) {
     await configDbPool.query('create database seacrifog;')
     await configDbPool.end()
     log('seacrifog database created!')
-
     seacrifogPool = getPool(process.env.POSTGRES_DATABASE || 'seacrifog')
     const schema = readFileSync(normalize(join(__dirname, './schema.sql')), { encoding: 'utf8' })
     await seacrifogPool.query(schema)
     await seacrifogPool.end()
     log('seacrifog schema created!')
+  } else {
+    log('starting app WITHOUT creating database and seeding schema (db already exists)')
   }
 
   return seacrifogPool || getPool(process.env.POSTGRES_DATABASE || 'seacrifog')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 var router = express.Router()
 
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
   res.send('hi')
 })
 
