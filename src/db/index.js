c8b387ea83966b4d61915c4330fa9c68b1ed15bb
Zach Smith <zachsmith.sa@gmail.com>
2019-08-16 16:14:52 +0200

    improved logging

--- src/db/index.js
+++ src/db/index.js
@@ -5,31 +5,32 @@ import { config } from 'dotenv'
 import { join, normalize } from 'path'
 config()
 
-const getPool = database => Pool({
-  host: process.env.POSTGRES_HOST || 'localhost',
-  user: process.env.POSTGRES_USER || 'postgres',
-  database,
-  password: process.env.POSTGRES_PASSWORD || 'password',
-  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432
-})
+const getPool = database =>
+  Pool({
+    host: process.env.POSTGRES_HOST || 'localhost',
+    user: process.env.POSTGRES_USER || 'postgres',
+    database,
+    password: process.env.POSTGRES_PASSWORD || 'password',
+    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432
+  })
 
 export default (async () => {
-  let seacrifogPool;
+  let seacrifogPool
   const configDbPool = getPool('postgres')
   const dbExists = (await configDbPool.query(
     `select exists(select datname from pg_catalog.pg_database where datname = 'seacrifog');`
   )).rows[0].exists
 
   if (!dbExists) {
-    log('Creating seacrifog database')
     await configDbPool.query('create database seacrifog;')
     await configDbPool.end()
+    log('seacrifog database created!')
 
     seacrifogPool = getPool(process.env.POSTGRES_DATABASE || 'seacrifog')
-    log('Creating seacrifog schema')
     const schema = readFileSync(normalize(join(__dirname, './schema.sql')), { encoding: 'utf8' })
     await seacrifogPool.query(schema)
     await seacrifogPool.end()
+    log('seacrifog schema created!')
   }
 
   return seacrifogPool || getPool(process.env.POSTGRES_DATABASE || 'seacrifog')
