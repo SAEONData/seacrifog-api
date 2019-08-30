c3cf586441c297087b2476a9e4f595604181ce26
Zach Smith <zachsmith.sa@gmail.com>
2019-08-30 09:18:52 +0200

    added redirect on the GET / route to the graphiql UI. This is for convinience during development

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 var router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.send('hi')
+  res.redirect('/graphiql')
 })
 
 export default router
