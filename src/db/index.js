a9ae6559528eb6324bfb3130d99f4cca295b8c13
Zach Smith <zachsmith.sa@gmail.com>
2019-09-18 13:31:17 +0200

    have to select columns explicitly from pg, since geometry type requires ST transformation functions. did this for single dataproduct query

--- src/db/index.js
+++ src/db/index.js
@@ -182,7 +182,38 @@ export const initializeLoaders = () => {
   })
 
   const findDataproducts = new DataLoader(async keys => {
-    const sql = `select * from public.dataproducts where id in (${keys.join(',')});`
+    const sql = `select
+    id,
+    title,
+    publish_year,
+    publish_date,
+    keywords,
+    abstract,
+    provider,
+    author,
+    contact,
+    ST_AsGeoJSON(coverage_spatial) coverage_spatial,
+    coverage_temp_start,
+    coverage_temp_end,
+    res_spatial,
+    res_spatial_unit,
+    res_temperature,
+    res_temperature_unit,
+    uncertainty,
+    uncertainty_unit,
+    doi,
+    license,
+    url_download,
+    file_format,
+    file_size,
+    file_size_unit,
+    url_info,
+    created_by,
+    created_at,
+    modified_by,
+    modified_at,
+    present
+    from public.dataproducts where id in (${keys.join(',')});`
     const rows = (await pool.query(sql)).rows
     return keys.map(key => rows.filter(sift({ id: key })) || [])
   })
