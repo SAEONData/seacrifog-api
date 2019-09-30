e0c1351618f42a6c47bd878d034757d3de7cc790
Zach Smith <zachsmith.sa@gmail.com>
2019-09-30 09:26:08 +0200

    added basic http api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,12 @@ router.get('/', async (req, res, next) => {
   res.redirect('/graphiql')
 })
 
+router.get('/http', async (req, res, next) => {
+  res.send('This is the HTTP API for SEACRIFOG')
+})
+
+router.get('/http/variables', async (req, res, next) => {
+  res.send('hi')
+})
+
 export default router
