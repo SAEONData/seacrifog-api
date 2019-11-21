329064dfa802acce88be932baaeca5ddc51b9da1
Zach Smith <zach@saeon.ac.za>
2019-11-21 13:56:23 +0200

    fixed bug where dataproducts of variables weren't being selected correctly

--- src/db/index.js
+++ src/db/index.js
@@ -263,7 +263,36 @@ export const initializeLoaders = () => {
   const findDataproductsOfVariables = new DataLoader(async keys => {
     const sql = `
     select
-    d.*,
+    d.id,
+    d.title,
+    d.publish_year,
+    d.publish_date,
+    d.keywords,
+    d.abstract,
+    d.provider,
+    d.author,
+    d.contact,
+    ST_AsGeoJSON(st_transform(d.coverage_spatial, 4326)) coverage_spatial,
+    d.coverage_temp_start,
+    d.coverage_temp_end,
+    d.res_spatial,
+    d.res_spatial_unit,
+    d.res_temperature,
+    d.res_temperature_unit,
+    d.uncertainty,
+    d.uncertainty_unit,
+    d.doi,
+    d.license,
+    d.url_download,
+    d.file_format,
+    d.file_size,
+    d.file_size_unit,
+    d.url_info,
+    d.created_by,
+    d.created_at,
+    d.modified_by,
+    d.modified_at,
+    d.present,    
     x.variable_id
     from public.dataproduct_variable_xref x
     join public.dataproducts d on d.id = x.dataproduct_id
