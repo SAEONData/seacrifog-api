3c65d31e167f02f1de711c26456b8e9317cc4a55
Zach Smith <zachsmith.sa@gmail.com>
2019-12-13 10:06:56 +0200

    Refactored query to be much cleaner

--- src/db/_query.js
+++ src/db/_query.js
@@ -1,23 +1,13 @@
 import { Query } from 'pg'
 
 export default async ({ pool, text, values, name }) => {
-  const client = await pool.connect()
+  let client
   try {
-    await new Promise((resolve, reject) => {
-      client.query(
-        new Query(
-          {
-            text,
-            values,
-            name
-          },
-          (err, result) => (err ? reject(err) : resolve(result))
-        )
-      )
-    })
+    client = await pool.connect()
+    await client.query({ text, values, name })
   } catch (error) {
     throw error
   } finally {
-    client.release()
+    if (client) client.release()
   }
 }
