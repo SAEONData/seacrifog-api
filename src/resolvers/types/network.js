c203444ef019907b5f26ec9c35614b8125f7dba0
Zach Smith <zachsmith.sa@gmail.com>
2019-12-17 13:14:45 +0200

    Fixed variable names that have changed

--- src/resolvers/types/network.js
+++ src/resolvers/types/network.js
@@ -2,8 +2,8 @@ import { log, logError } from '../../lib/log'
 
 export default {
   variables: async (self, args, req) => {
-    const { findVariablesOfNetworks } = req.ctx.db.dataLoaders
-    const result = await findVariablesOfNetworks(self.id)
+    const { findVariablesOfNetwork } = req.ctx.db.dataLoaders
+    const result = await findVariablesOfNetwork(self.id)
     return result.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
   }
 }
--- src/resolvers/types/radiative-forcing.js
+++ src/resolvers/types/radiative-forcing.js
@@ -2,8 +2,8 @@ import { log, logError } from '../../lib/log'
 
 export default {
   variables: async (self, args, req) => {
-    const { findVariablesOfRadiativeForcings } = req.ctx.db.dataLoaders
-    const result = await findVariablesOfRadiativeForcings(self.id)
+    const { findVariablesOfRadiativeForcing } = req.ctx.db.dataLoaders
+    const result = await findVariablesOfRadiativeForcing(self.id)
     return result.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
   }
 }
