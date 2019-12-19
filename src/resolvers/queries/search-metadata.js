aed8382637a1e08790339263017a0f77fd09e4ed
Zach Smith <zachsmith.sa@gmail.com>
2019-12-19 14:34:38 +0200

    Updated the API to send back a real count of metadata records found, searching for them by network, protocol, variable categories

--- src/resolvers/queries/search-metadata.js
+++ src/resolvers/queries/search-metadata.js
@@ -2,14 +2,30 @@ import { log, logError } from '../../lib/log'
 import axios from 'axios'
 
 export default async (self, args, req) => {
-  const { byVariables = [], byProtocols = [], bySites = [], byNetworks = [] } = args
+  const { findNetworks, findVariables, findProtocols } = req.ctx.db.dataLoaders
+  const { byNetworks = [], byVariables = [], byProtocols = [] } = args
+
+  // Get networks search terms
+  const networks = await Promise.all(byNetworks.map(async id => (await findNetworks(id))[0]))
+  const ns = networks.map(n => n.type)
+
+  // Get variables search terms
+  const variables = await Promise.all(byVariables.map(async id => (await findVariables(id))[0]))
+  const vs = variables.map(v => v.domain)
+
+  // Get protocols search terms
+  const protocols = await Promise.all(byProtocols.map(async id => (await findProtocols(id))[0]))
+  const ps = protocols.map(p => p.domain)
+
+  const search = [...new Set([...ns, ...vs, ...ps])]
+
   const { data } = await axios({
     baseURL: 'http://192.168.116.66:9210/search',
     params: {
       index: 'saeon-odp-4-2',
-      size: 100,
+      size: 10000,
       fields: 'metadata_json,record_id,organization',
-      'metadata_json.subjects.subject': 'fire,wind'
+      'metadata_json.subjects.subject': search.join(',')
     },
     method: 'GET',
     headers: { 'Content-Type': 'application/json' }
@@ -18,5 +34,5 @@ export default async (self, args, req) => {
     throw error
   })
 
-  return [{ id: 1 }]
+  return data.results.map((item, i) => ({ id: i + 1 }))
 }
