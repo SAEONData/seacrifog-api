fcb836897fa5382be99185e06b1a9c5f890776b4
Zach Smith <zachsmith.sa@gmail.com>
2020-02-06 14:35:58 +0200

    Updated the API to use the correct query - but it's not dynamic yet

--- src/resolvers/queries/search-metadata/executors/_icos.mjs
+++ src/resolvers/queries/search-metadata/executors/_icos.mjs
@@ -10,22 +10,33 @@ import axios from 'axios'
       'accept-encoding': 'gzip, deflate, br'
     },
     data: `
+    # extendedDataObjectInfo
     prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
     prefix prov: <http://www.w3.org/ns/prov#>
-    
-    select
-    (str(?submTime) as ?time)
-    ?dobj
-    ?spec
-    ?dataLevel
-    ?fileName
-    
-    where {
-      ?dobj cpmeta:hasName ?fileName .
-      ?dobj cpmeta:hasObjectSpec [rdfs:label ?spec ; cpmeta:hasDataLevel ?dataLevel].
-    }
-    order by desc(?submTime)
-    limit 1000`
+    select distinct ?dobj ?station ?stationId ?samplingHeight ?theme ?themeIcon ?title ?description ?columnNames ?site where{
+      {
+        select ?dobj (min(?station0) as ?station) (sample(?stationId0) as ?stationId) (sample(?samplingHeight0) as ?samplingHeight) (sample(?site0) as ?site) where{
+          VALUES ?dobj { <https://meta.icos-cp.eu/objects/Sb2xxbU06-oeCC9ITKxTRdWk> <https://meta.icos-cp.eu/objects/x2gL6BMqkQEbh928i1roE3ky> <https://meta.icos-cp.eu/objects/FrtCNzUnRdup-_hau8K1DZh-> <https://meta.icos-cp.eu/objects/Su3qm3JOhpbGAk8fIJ3gxSRA> <https://meta.icos-cp.eu/objects/rVbJrJG6fv7t53cLMvmId-MV> }
+          OPTIONAL{
+            ?dobj cpmeta:wasAcquiredBy ?acq.
+            ?acq prov:wasAssociatedWith ?stationUri .
+            OPTIONAL{ ?stationUri cpmeta:hasName ?station0 }
+            OPTIONAL{ ?stationUri cpmeta:hasStationId ?stationId0 }
+            OPTIONAL{ ?acq cpmeta:hasSamplingHeight ?samplingHeight0 }
+            OPTIONAL{ ?acq cpmeta:wasPerformedAt/cpmeta:hasSpatialCoverage/rdfs:label ?site0 }
+          }
+        }
+        group by ?dobj
+      }
+      ?dobj cpmeta:hasObjectSpec ?specUri .
+      OPTIONAL{ ?specUri cpmeta:hasDataTheme [
+        rdfs:label ?theme ;
+        cpmeta:hasIcon ?themeIcon
+      ]}
+      OPTIONAL{ ?dobj <http://purl.org/dc/terms/title> ?title }
+      OPTIONAL{ ?dobj <http://purl.org/dc/terms/description> ?description }
+      OPTIONAL{?dobj cpmeta:hasActualColumnNames ?columnNames }
+    }`
   }
 
   console.log('ICOS Metadata search', options)
